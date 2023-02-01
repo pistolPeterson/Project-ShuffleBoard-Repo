60a40694a1a986dbbb29fb6404251b67bdc3f8db
PetersonNormil <petersonnormil@gmail.com>
2023-02-01 16:22:15 -0500

    update score system, made it much cooler than what rae did

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -3,23 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// summary?
+/// </summary>
 public class ScoreSystem : MonoBehaviour
 {
-    private int randomScore = 0;
+    //TODO Raeus mf
+    //add a mf summary 
+    //remove the testing stuff 
     private int currentScore = 0;
-
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private int pointsLowerLimit = -5;
     [SerializeField] private int pointsUpperLimit = 10;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            randomScore = Random.Range(pointsLowerLimit, pointsUpperLimit);
-            Debug.Log("RS:" + randomScore);
-
-            currentScore += randomScore;
-            scoreText.text = "Score: " + currentScore;
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+           int randomScore = Random.Range(pointsLowerLimit, pointsUpperLimit);
+           UpdateScore(randomScore);   
         }     
+    }
+
+    public void UpdateScore(int addedScore)
+    {
+        currentScore += addedScore;
+        scoreText.text = "Score: " + currentScore;
     }
 }
